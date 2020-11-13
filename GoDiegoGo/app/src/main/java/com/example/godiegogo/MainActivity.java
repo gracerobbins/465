e6e9f17b18517c4a3914a2eef969e8f34288f976
gracerobbins <gcrobbins2@gmail.com>
2020-11-13 14:14:15 -0600

    template files added

---
+++ GoDiegoGo/app/src/main/java/com/example/godiegogo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.godiegogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.graphics.Color;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                button.setBackgroundColor(Color.BLUE);
+            }
+        });
+    }
+}
