73e5b50e4b530b6edbb7f293186384a7888732f9
gracerobbins <gcrobbins2@gmail.com>
2020-11-22 02:06:13 -0600

    minor updates to main view

--- GoDiegoGo/app/src/main/java/com/example/godiegogo/MainActivity.java
+++ GoDiegoGo/app/src/main/java/com/example/godiegogo/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.godiegogo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.graphics.Color;
 import android.content.Intent;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.ImageButton;
@@ -28,6 +31,12 @@ public class MainActivity extends AppCompatActivity {
         playlist_names.add("Playlist Name 2");
         playlist_names.add("Playlist Name 3");
         playlist_names.add("Playlist Name 4");
+
+
+        ListView lv = (ListView) findViewById(R.id.playlist_selector);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playlist_names);
+        lv.setAdapter(itemsAdapter);
 
         final Button transfer_button = findViewById(R.id.transfer_button);
         transfer_button.setOnClickListener(new View.OnClickListener() {
