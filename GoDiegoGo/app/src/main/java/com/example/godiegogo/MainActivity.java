22472193bd8a7dc6d022534174e08b0fcddb65ce
gracerobbins <gcrobbins2@gmail.com>
2020-11-22 14:03:51 -0600

    making transfer menu playlists selectable with a checklist

--- GoDiegoGo/app/src/main/java/com/example/godiegogo/MainActivity.java
+++ GoDiegoGo/app/src/main/java/com/example/godiegogo/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.godiegogo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.BlendMode;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
 import android.graphics.Color;
 import android.content.Intent;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import android.os.Bundle;
 
@@ -31,12 +38,32 @@ public class MainActivity extends AppCompatActivity {
         playlist_names.add("Playlist Name 2");
         playlist_names.add("Playlist Name 3");
         playlist_names.add("Playlist Name 4");
+        playlist_names.add("Playlist Name 2");
+        playlist_names.add("Playlist Name 3");
+        playlist_names.add("Playlist Name 4");
+        playlist_names.add("Playlist Name 2");
+        playlist_names.add("Playlist Name 3");
+        playlist_names.add("Playlist Name 4");
+        playlist_names.add("Playlist Name 2");
+        playlist_names.add("Playlist Name 3");
+        playlist_names.add("Playlist Name 4");
 
-
-        ListView lv = (ListView) findViewById(R.id.playlist_selector);
+        GridView grid_view = (GridView) findViewById(R.id.playlist_selector);
         ArrayAdapter<String> itemsAdapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playlist_names);
-        lv.setAdapter(itemsAdapter);
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, playlist_names);
+        grid_view.setAdapter(itemsAdapter);
+        grid_view.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                CheckedTextView checkedTextView = ((CheckedTextView)view);
+                int[][] states = new int[][] {new int[] { android.R.attr.state_enabled} };//enabled state
+                int[] colors = new int[] { Color.BLACK };
+                ColorStateList color_states = new ColorStateList(states, colors);
+                checkedTextView.setCheckMarkTintList(color_states);
+                checkedTextView.setChecked(!checkedTextView.isChecked());
+            }
+        });
 
         final Button transfer_button = findViewById(R.id.transfer_button);
         transfer_button.setOnClickListener(new View.OnClickListener() {
