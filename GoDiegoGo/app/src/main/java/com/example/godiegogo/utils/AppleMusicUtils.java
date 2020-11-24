3c712630bbac78d7cb32425f5e52aa680209acb2
Diego Hernandez Nater <dhernandeznater@gmail.com>
2020-11-24 14:26:05 -0600

    Cleaned up AppleMusicUtils.java and simplified API call methods into a single method

--- GoDiegoGo/app/src/main/java/com/example/godiegogo/utils/AppleMusicUtils.java
+++ GoDiegoGo/app/src/main/java/com/example/godiegogo/utils/AppleMusicUtils.java
@@ -66,42 +66,18 @@ public class AppleMusicUtils {
         return songList;
     }
 
-    // Add a list of JSON formatted songs to a playlist
-    public static void addSongsToPlaylist(Context context, JSONObject songs, String playlistId, final VolleyResponseListener listener) {
-        String url = "https://api.music.apple.com/v1/me/library/playlists/" + playlistId + "/tracks";
-
-        RequestQueue queue = Volley.newRequestQueue(context);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, songs, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                listener.onResponse(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                listener.onResponse(error);
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("Authorization", "Bearer " + context.getString(R.string.jwt_token));
-                params.put("Music-User-Token", ApplePreferences.with(context).getUserToken());
-                return params;
-            }
-        };
-
-        queue.add(jsonObjectRequest);
+    // Generate the URL for a song search
+    public static String generateSongSearchURL(String searchTerm) {
+        String formattedSearchTerm = searchTerm.replace(' ', '+');
+        return "https://api.music.apple.com/v1/catalog/us/search?term=" + formattedSearchTerm + "&types=songs";
     }
 
-    // Search for a song with the Apple Music API
-    public static void searchForSong(Context context, String songName, final VolleyResponseListener listener) {
-        String formattedSong = songName.replace(' ', '+');
-        Log.d("song search", "Formatted Song: " + formattedSong);
-        String url = "https://api.music.apple.com/v1/catalog/us/search?term=" + formattedSong + "&types=songs";
-        RequestQueue queue = Volley.newRequestQueue(context);
+    // Make an API request to Apple Music API
+    public static void makeApiRequest(Context context, int requestMethod, JSONObject jsonObjectForRequest, String url, final VolleyResponseListener listener) {
+        Log.d("Api Request", "Method: " + requestMethod + " - Url: " + url);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(requestMethod, url, jsonObjectForRequest, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 listener.onResponse(response);
@@ -120,103 +96,7 @@ public class AppleMusicUtils {
                 return params;
             }
         };
-
         queue.add(jsonObjectRequest);
-    }
-
-    public static JSONObject synchronousSearchForSong(Context context, String songName) {
-        String formattedSong = songName.replace(' ', '+');
-        Log.d("song search", "Formatted Song: " + formattedSong);
-        String url = "https://api.music.apple.com/v1/catalog/us/search?term=" + formattedSong + "&types=songs";
-        RequestQueue queue = Volley.newRequestQueue(context);
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, new JSONObject(), future, future) {
-            @Override
-            public Map<String, String> getHeaders() {
-                Map<String, String> params = new HashMap<>();
-                params.put("Authorization", "Bearer " + context.getString(R.string.jwt_token));
-                params.put("Music-User-Token", ApplePreferences.with(context).getUserToken());
-                return params;
-            }
-        };
-
-        queue.add(jsonObjectRequest);
-        try {
-
-            JSONObject response = future.get(5, TimeUnit.SECONDS);
-            return response;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-
-    public static void addNewPlaylist(Context context, JSONObject playlist, final VolleyResponseListener listener) throws JSONException {
-        String url = "https://api.music.apple.com/v1/me/library/playlists";
-
-        RequestQueue queue = Volley.newRequestQueue(context);
-
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, playlist, new Response.Listener<JSONObject>()
-        {
-            @Override
-            public void onResponse(JSONObject response) {
-                listener.onResponse(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                listener.onError("VolleyError: " + error.toString());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("Authorization", "Bearer " + context.getString(R.string.jwt_token));
-                params.put("Music-User-Token", ApplePreferences.with(context).getUserToken());
-                return params;
-            }
-        };
-
-        queue.add(jsonObjectRequest);
-
-    }
-
-    public static void getAppleMusicPlaylists(Context context, final VolleyResponseListener listener) throws JSONException, IllegalStateException {
-        String userToken = ApplePreferences.with(context).getUserToken();
-
-        if (userToken == null || userToken.isEmpty()) {
-            throw new IllegalStateException();
-        }
-
-        RequestQueue queue = Volley.newRequestQueue(context);
-
-        String url = "https://api.music.apple.com/v1/me/library/playlists?limit=100";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                listener.onResponse(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                listener.onError("VolleyError: " + error.toString());
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<>();
-                params.put("Authorization", "Bearer " + context.getString(R.string.jwt_token));
-                params.put("Music-User-Token", ApplePreferences.with(context).getUserToken());
-                return params;
-            }
-        };
-
-        queue.add(stringRequest);
     }
 
 
