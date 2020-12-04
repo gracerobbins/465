bc0fce5462785c53252b7974d7ceb68590649aa2
Chaitanya Jujjavarapu <cnj3@illinois.edu>
2020-12-03 19:19:42 -0600

    committing

--- GoDiegoGo/app/src/main/java/com/example/godiegogo/SearchConfirmActivity.java
+++ GoDiegoGo/app/src/main/java/com/example/godiegogo/SearchConfirmActivity.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.godiegogo.preferences.SpotifyPreferences;
@@ -20,28 +21,38 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.FormBody;
 import okhttp3.Response;
+
+import static android.os.Build.VERSION_CODES.O;
 
 
 public class SearchConfirmActivity extends AppCompatActivity {
     public ArrayList<String> playlist_songs;
     public ArrayList<String> playlist_url;
     public String url;
-    private String accessToken;
+    public String accessToken;
     private static final String TAG = "MyActivity";
     private Call mCall;
     private Call mCall_name;
+    private Call mCall_token;
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private final OkHttpClient mOkHttpClient_name = new OkHttpClient();
+    private final OkHttpClient mOkHttpClient_token = new OkHttpClient();
     private ArrayAdapter<String> itemsAdapter;
     private String playlistName;
     public String playlistId;
+    private String CLIENTID = "17c1c4ef3eda4cd0afa1abe42019cac7";
+    private String CLIENTSECRET = "ed322c47e9f542d6b905cb33187d6958";
 
+    @RequiresApi(api = O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,13 +64,62 @@ public class SearchConfirmActivity extends AppCompatActivity {
 
         Bundle b = this.getIntent().getExtras();
         url = b.getString("url");
-        accessToken = SpotifyPreferences.with(getApplicationContext()).getUserToken();
-        if (url.contains("spotify") && accessToken != null) {
+
+//        } else {
+//            accessToken = SpotifyPreferences.with(getApplicationContext()).getUserToken();
+//        }
+
+
+
+        if (url.contains("spotify") ) {
+
+
+            String originalInput = CLIENTID + ":" + CLIENTSECRET;
+            System.out.println(CLIENTID);
+            System.out.println(CLIENTSECRET);
+            String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+
+//            final RequestBody formBody = new FormBody.Builder()
+//                    .add("grant_type", "client_credentials")
+//                    .build();
+            final RequestBody formBody = new FormBody.Builder()
+                    .add("grant_type", "client_credentials")
+                    .build();
+
+            final Request request_token = new Request.Builder()
+                    .url("https://accounts.spotify.com/api/token")
+                    .addHeader("Authorization", "Basic " + encodedString)
+//                    .addHeader("grant_type", "client_credentials")
+                    .post(formBody)
+                    .build();
+
+            mCall_token = mOkHttpClient_token.newCall(request_token);
+            mCall_token.enqueue(new Callback() {
+                public void onFailure(Call call, IOException e) {
+                    System.out.println(e);
+                    Log.d("Failure", "onFailureMethodCalled_token");
+                }
+
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        final JSONObject jsonObject = new JSONObject(response.body().string());
+                        accessToken = jsonObject.getString("access_token");
+                        System.out.println("access_token hit");
+                        Log.d("accessToken", accessToken);
+                    } catch (JSONException e) {
+                        System.out.println(e);
+                        Log.d("JSONException_token", "it hit this token");
+//                        setResponse("Failed to parse data: " + e);
+                    }
+                }
+            });
+
             // do spotify stuff
             playlistId = url.substring(url.lastIndexOf("/") + 1);
             if (playlistId.contains("?")) {
                 playlistId = playlistId.substring(0, playlistId.indexOf("?"));
             }
+
             final Request request = new Request.Builder()
                     .url("https://api.spotify.com/v1/playlists/" + playlistId + "/tracks?limit=10")
                     .addHeader("Authorization","Bearer " + accessToken)
@@ -110,9 +170,8 @@ public class SearchConfirmActivity extends AppCompatActivity {
                         for (int i = 0; i < items.length(); i++) {
                             JSONObject p = items.getJSONObject(i).getJSONObject("track");
                             Log.d("p", "it is here");
-//                            System.out.println(p);
                             playlist_songs.add(p.getString("name"));
-//                            Log.d("add", "it is here");
+
 //                            JSONObject extURL = items.getJSONObject(i).getJSONObject("external_urls");
 //                            playlist_url.add(extURL.getString("spotify"));
 
@@ -157,8 +216,6 @@ public class SearchConfirmActivity extends AppCompatActivity {
                    ArrayList<String> playlist_ids = new ArrayList<String>();
                    playlist_ids.add(playlistId);
 
-                   System.out.println(playlists);
-
                    Bundle b = new Bundle();
                    b.putStringArrayList("checked_playlists", playlists);
                    b.putStringArrayList("checked_playlist_ids", playlist_ids);
@@ -181,12 +238,15 @@ public class SearchConfirmActivity extends AppCompatActivity {
     }
 
     private void cancelCall() {
-        if (mCall != null) {
-            mCall.cancel();
-        }
-        if (mCall_name != null) {
-            mCall_name.cancel();
-        }
+//        if (mCall != null) {
+//            mCall.cancel();
+//        }
+//        if (mCall_name != null) {
+//            mCall_name.cancel();
+//        }
+////        if (mCall_token != null) {
+//            mCall_token.cancel();
+//        }
     }
 
 }
